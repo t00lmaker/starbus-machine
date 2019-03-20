e47f0faea5a9867e8b175b8ef6eb166e54da5ecb
luan <luan@infoway-pi.com.br>
2019-03-19 22:25:24 -0300

    implements migrations

--- apps/router/priv/repo/tenant_migrations/20190320010403_create_route.exs
+++ apps/router/priv/repo/tenant_migrations/20190320010403_create_route.exs
@@ -2,6 +2,12 @@ defmodule Router.Repo.Migrations.CreateRoute do
   use Ecto.Migration
 
   def change do
+    create table(:routes) do
+      add :name, :string
+      add :info, :text
+      add :user_id, references(:users)
 
+      timestamps()
+    end
   end
 end
--- apps/router/priv/repo/tenant_migrations/20190320010436_create_points_routes.exs
+++ apps/router/priv/repo/tenant_migrations/20190320010436_create_points_routes.exs
@@ -2,6 +2,9 @@ defmodule Router.Repo.Migrations.CreatePointsRoutes do
   use Ecto.Migration
 
   def change do
-
+    create table(:points_routes, primary_key: false) do
+      add :point_id, references(:points)
+      add :route_id, references(:routes)
+    end
   end
 end

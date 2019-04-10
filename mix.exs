ba8505ae3b51a6f9763755452e1dd3bb69cf5305
luan <luan@infoway-pi.com.br>
2019-04-09 23:09:35 -0300

    ajuste database db uri

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Starbus.MixProject do
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      test_coverage: [tool: ExCoveralls]]
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
